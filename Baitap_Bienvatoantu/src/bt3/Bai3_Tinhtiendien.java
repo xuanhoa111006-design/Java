59f5385a36cd9b66c0742554e241f0463589aa63
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 11:54:54 +0700

    Điều kiện tính tiền điện

--- Baitap_Bienvatoantu/src/bt3/Bai3_Tinhtiendien.java
+++ Baitap_Bienvatoantu/src/bt3/Bai3_Tinhtiendien.java
@@ -6,6 +6,17 @@ public class Bai3_Tinhtiendien {
 		// TODO Auto-generated method stub
 		 int soDien = 250;  // kWh
 	     double tienDien = 0;
+	     if (soDien <= 50) {
+	            tienDien = soDien * 1678;
+	        } else if (soDien <= 100) {
+	            tienDien = 50 * 1678 + (soDien - 50) * 1734;
+	        } else if (soDien <= 200) {
+	            tienDien = 50 * 1678 + 50 * 1734 + (soDien - 100) * 2014;
+	        } else if (soDien <= 300) {
+	            tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + (soDien - 200) * 2536;
+	        } else {
+	            tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + (soDien - 300) * 2834;
+	        }
 	}
 
 }

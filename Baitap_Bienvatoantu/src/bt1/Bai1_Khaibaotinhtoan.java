b697538dbeb7ba4b66d40cf0c3f2c31e42a2458c
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 11:50:46 +0700

    Tính toán và in kết quả

--- Baitap_Bienvatoantu/src/bt1/Bai1_Khaibaotinhtoan.java
+++ Baitap_Bienvatoantu/src/bt1/Bai1_Khaibaotinhtoan.java
@@ -7,7 +7,14 @@ public class Bai1_Khaibaotinhtoan {
 		int a = 15;
         int b = 4;
         double c = 3.5;
-
+        // 2. Tính toán
+        int tong = a + b;
+        double thuong = a / c;
+        int phanDu = a ;
+        // 3. In kết quả tính
+        System.out.println("Tổng: " + tong);
+        System.out.println("Thương: " + thuong);
+        System.out.println("Phần dư: " + phanDu);
 	}
 
 }

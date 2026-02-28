dceab5d6b981e16f60f8c95fb9158dbe8566cb44
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 11:57:11 +0700

    Nhập dữ liệu từ bàn phím

--- Baitap_Scanner/src/Bai1_Tinhhaiso.java
+++ Baitap_Scanner/src/Bai1_Tinhhaiso.java
@@ -3,7 +3,13 @@ public class Bai1_Tinhhaiso {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập số thứ nhất: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Nhập số thứ hai: ");
+        double num2 = scanner.nextDouble();
 	}
 
 }

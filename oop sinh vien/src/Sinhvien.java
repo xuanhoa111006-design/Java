94b40a1252eb0f1382420de46dacc79550e5ea51
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 12:14:15 +0700

    Tạo class sinh viên

---
+++ oop sinh vien/src/Sinhvien.java
@@ -0,0 +1,14 @@
+
+public class Sinhvien {
+
+	
+		// TODO Auto-generated method stub
+		String ten;
+		String mssv;
+		int tuoi;
+		// Định nghĩa các phương thức / hàm
+			void hocBai(String monhoc) {
+			System.out.print("Bạn " + ten + " đang học môn " + monhoc);
+	}
+
+}

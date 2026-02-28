5c60790464d7ff0ad3f3f2b77774d242897fc17d
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 11:53:37 +0700

    Điều kiện và tính in ra chỉ số BMI

--- Baitap_Bienvatoantu/src/bt3/Bai3_TinhBMI.java
+++ Baitap_Bienvatoantu/src/bt3/Bai3_TinhBMI.java
@@ -12,6 +12,15 @@ public class Bai3_TinhBMI {
         System.out.println("Cân nặng: " + weight + " kg");
         System.out.println("Chiều cao: " + height + " m");
         System.out.println("Chỉ số BMI: " + bmi);
+        if (bmi < 18.5) {
+            System.out.println("Phân loại: Thiếu cân");
+        } else if (bmi < 25) {
+            System.out.println("Phân loại: Bình thường");
+        } else if (bmi < 30) {
+            System.out.println("Phân loại: Thừa cân");
+        } else {
+            System.out.println("Phân loại: Béo phì");
+        }
 	}
 
 }

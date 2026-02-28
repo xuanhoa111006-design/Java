62e9f19c1b1cba247567326696b2d5f9ba75ada4
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 12:10:49 +0700

    Nhập thông tin

--- Baitap_Scanner/src/Bai3_Qlysinhvien.java
+++ Baitap_Scanner/src/Bai3_Qlysinhvien.java
@@ -6,7 +6,25 @@ public class Bai3_Qlysinhvien {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("=== NHẬP THÔNG TIN SINH VIÊN ===");
+        System.out.print("Mã sinh viên: ");
+        String id = scanner.nextLine();
+        
+        System.out.print("Họ tên: ");
+        String name = scanner.nextLine();
+        
+        System.out.print("Điểm Toán: ");
+        double math = scanner.nextDouble();
+        
+        System.out.print("Điểm Lý: ");
+        double physics = scanner.nextDouble();
+        
+        System.out.print("Điểm Hóa: ");
+        double chemistry = scanner.nextDouble();
+        
+        double average = (math + physics + chemistry) / 3;
 	}
 
 }

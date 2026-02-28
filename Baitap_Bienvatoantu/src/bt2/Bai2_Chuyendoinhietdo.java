c6abc3332752c4d8b80c1c1ab4d775cf93743e92
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 11:51:52 +0700

    Tạo file và khai báo các biến bài 2

---
+++ Baitap_Bienvatoantu/src/bt2/Bai2_Chuyendoinhietdo.java
@@ -0,0 +1,11 @@
+package bt2;
+
+public class Bai2_Chuyendoinhietdo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double celsius = 25.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+	}
+
+}

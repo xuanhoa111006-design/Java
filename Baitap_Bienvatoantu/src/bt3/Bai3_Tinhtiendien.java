a3a790cc92786e6c14beeb5eb8d981ecdc7d4d48
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 11:54:36 +0700

    Tạo file khai báo biến bài 3

---
+++ Baitap_Bienvatoantu/src/bt3/Bai3_Tinhtiendien.java
@@ -0,0 +1,11 @@
+package bt3;
+
+public class Bai3_Tinhtiendien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int soDien = 250;  // kWh
+	     double tienDien = 0;
+	}
+
+}

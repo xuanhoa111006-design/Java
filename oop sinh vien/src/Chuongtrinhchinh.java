9a88c4879caeee902672f5a7a48afaf5929950ff
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 12:15:24 +0700

    Tạo file chương trình chính và tạo ra 1  sinh viên

---
+++ oop sinh vien/src/Chuongtrinhchinh.java
@@ -0,0 +1,15 @@
+
+public class Chuongtrinhchinh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Tạo mới 1 sinh viên
+				Sinhvien sv1 = new Sinhvien();
+				// cấp/gán dữ liệu cụ thể
+						sv1.mssv="66123456";
+						sv1.ten="Luon Vui Tuoi";
+						sv1.tuoi=18;
+						sv1.hocBai("Lập trình Java");
+	}
+
+}

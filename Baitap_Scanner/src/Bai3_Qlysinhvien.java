03b97580d3fb519e2a1d08bdfac1a55a9b4b2e19
xuanhoa111006-design <xuanhoa111006@gmail.com>
2026-02-28 12:10:21 +0700

    Tạo file và import các lớp

---
+++ Baitap_Scanner/src/Bai3_Qlysinhvien.java
@@ -0,0 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Bai3_Qlysinhvien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
